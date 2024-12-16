a5fe833bb6991b2b0d431d291012d728e521d6a6
Timo Leenen <leenen@adobe.com>
2024-12-16 16:04:45 +0100

    - Fixed indentation - More specific div selector for icon span

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,7 +9,9 @@ export default function decorate(block) {
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) { div.className = 'cards-card-image'; } else if (div.children.length === 1 && div.querySelector('p')) {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else if (div.children.length === 1 && div.querySelector('p') && div.querySelector('p').textContent.startsWith('icon')) {
         div.className = 'cards-card-icon';
         const iconSpan = document.createElement('span');
         const p = div.querySelector('p');
